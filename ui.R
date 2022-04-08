73a7011a5c26c915b7a37e68556b70a6515a473e
1ndy <hjdemeyer64@gmail.com>
2022-04-08 11:49:41 -0500

    Fix typo in help text

--- ui.R
+++ ui.R
@@ -6,7 +6,7 @@ helptext <- function() {
     h1("Bt Simulation"),
     h2("About"),
     p("Bt simulation models the effect of bacillus thuringenesis-derived 
-      toxins in transgenic corn agasint the western corn rootworm. It relies on
+      toxins in transgenic corn against the western corn rootworm. It relies on
       principles from population genetics to compute population level changes
       in census number and allele frequency."),
     br(),
