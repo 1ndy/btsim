81ff00205c32b28694c830f2e5c70f1f13bcdeaf
Hunter DeMeyer <33045591+1ndy@users.noreply.github.com>
2022-04-09 19:51:07 -0500

    Account for different loss in Bt and refuge plants

--- R/sim.R
+++ R/sim.R
@@ -1,5 +1,13 @@
-calc_yield_loss_percent <- function(pest_density) {
-  loss = (.765*pest_density)/100
+calc_yield_loss <- function(pest_density, percent_refuge, recessive_genotype_count, plant_density, acres, bpa) {
+  refuge_loss = min(((.765*pest_density)/100), 1)
+  bt_res_pest_density = 0
+  if(percent_refuge==1) {
+    bt_res_pest_density=recessive_genotype_count/(acres*plant_density)
+  } else {
+    bt_res_pest_density=recessive_genotype_count/((1-percent_refuge)*acres*plant_density)
+  }
+  bt_loss = min(((.765*bt_res_pest_density)/100), 1)
+  loss = refuge_loss*bpa*percent_refuge + bt_loss*bpa*(1-percent_refuge)
   return(loss)
 }
 
@@ -67,20 +75,20 @@ gen_survivors <- function(n_insects, res_allele_freq, percent_refuge, pest_densi
   # P(not on refuge plant)
   death_causing_plants = rbinom(1, total_plants, percent_bt)
   deaths = (p_sq * pest_density) * death_causing_plants
-  new_n_insects = n_insects - deaths #max(bt_sus_insects - deaths + recessive_genotype_count,0)
+  new_n_insects = n_insects - deaths
   new_n_insects = round(calc_offspring_count(new_n_insects),0)
   new_pest_density = new_n_insects / (acreage * plant_density)
-  yield_loss = calc_yield_loss_percent(new_pest_density)
+  yield_loss = calc_yield_loss(pest_density, percent_refuge, recessive_genotype_count, plant_density, acreage, bpa)
   new_raf = 0
   if(1-res_allele_freq != 1 && new_n_insects > 0) {
     new_raf = (((h_sq / 2) * w12)+(q_sq*w22)) / (p_sq*w11 + h_sq*w12 + q_sq*w22)
   }
-  r = c(new_n_insects, new_raf, percent_refuge, new_pest_density, calc_crop_yield(bpa, calc_crop_loss(yield_loss, percent_refuge, bpa)))
+  r = c(new_n_insects, new_raf, percent_refuge, new_pest_density, bpa-yield_loss)
   return(r)
 }
 
 calc_generations <- function(num_gens, num_insects, raf, rp, pest_density, bpa, acreage, plant_density, h, s, selection_mode, df) {
-  df[1, ] <- c(1,num_insects,raf,rp,pest_density,bpa * (1 - calc_yield_loss_percent(pest_density)))
+  df[1, ] <- c(1,num_insects,raf,rp,pest_density,bpa - calc_yield_loss(pest_density, rp, (raf^2*num_insects), plant_density, acreage, bpa))
   for(i in 2:num_gens) {
     result = gen_survivors(df[i-1,2], df[i-1, 3], rp, df[i-1, 5], bpa, acreage, plant_density, h, s, selection_mode, df)
     df[i, ] = c(i,result)
@@ -91,15 +99,15 @@ calc_generations <- function(num_gens, num_insects, raf, rp, pest_density, bpa, 
 
 simulate <- function(generations=10,n_insects=100000,resistant_allele_freq=0,refuge_crop_percentage=0.2,bushels_per_acre=150, acreage=10, plant_density=25000, h=0.5, s=0.5, selection_mode="darR") {
   pest_density = n_insects / (acreage * plant_density)
-  
+
   df <- data.frame(generation=integer(),
                    n_pests=integer(),
                    res_allele_freq=double(),
                    percent_refuge=double(),
                    pest_density=integer(),
                    bushels_per_acre=double())
-  
-  
+
+
   decline = calc_generations(generations,
                              n_insects,
                              resistant_allele_freq,
